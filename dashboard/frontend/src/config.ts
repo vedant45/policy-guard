a6e1dbcc6a0f6772db2791dcc4b89b7e8c6e9588
Vedant Balachandra <vedantbalachandra123@gmail.com>
2026-04-26 21:12:05 +1000

    changed config

--- dashboard/frontend/src/config.ts
+++ dashboard/frontend/src/config.ts
@@ -1,6 +1,6 @@
-// export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-// export const WS_URL = import.meta.env.VITE_WS_URL || "ws://localhost:8080";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+export const WS_URL = import.meta.env.VITE_WS_URL || "ws://localhost:8080";
 
 
-export const API_URL = "https://sentari.onrender.com";
-export const WS_URL = "wss://sentari.onrender.com";
+// export const API_URL = "https://sentari.onrender.com";
+// export const WS_URL = "wss://sentari.onrender.com";
